893d58b7c63216859055b98e10f93428b4adcf45
AdityaParmale <adityaparmale20@gmail.com>
2025-12-01 22:54:28 +0530

    Update FakesNewsDetectModel.py

--- FakesNewsDetectModel.py
+++ FakesNewsDetectModel.py
@@ -1,6 +1,8 @@
 import numpy as np
 import pandas as pd
 import re
+import nltk
+nltk.data.path.append('./nltk_data')
 from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
 from sklearn.feature_extraction.text import TfidfVectorizer
